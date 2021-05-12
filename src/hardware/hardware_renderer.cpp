584e18136904dc476e0acef65d25c7ce59084245
AnotherNoobDev <gebesleaga@gmail.com>
2021-05-12 17:21:24 +0200

    completed task 1

--- src/hardware/hardware_renderer.cpp
+++ src/hardware/hardware_renderer.cpp
@@ -246,6 +246,10 @@ void HardwareRenderer::rasterize_point(float x, float y, Color color) {
   // Task 1: 
   // Implement point rasterization
 
+  glBegin(GL_POINTS);
+  glColor4f(color.r, color.g, color.b, color.a);
+  glVertex2f(x, y);
+  glEnd();
 }
 
 void HardwareRenderer::rasterize_line(float x0, float y0,
@@ -254,7 +258,12 @@ void HardwareRenderer::rasterize_line(float x0, float y0,
 
   // Task 1: 
   // Implement line rasterization
-
+  
+  glBegin(GL_LINES);
+  glColor4f(color.r, color.g, color.b, color.a);
+  glVertex2f(x0, y0);
+  glVertex2f(x1, y1);
+  glEnd();
 }
 
 void HardwareRenderer::rasterize_triangle(float x0, float y0,
@@ -263,7 +272,12 @@ void HardwareRenderer::rasterize_triangle(float x0, float y0,
                                           Color color) {
   // Task 1: 
   // Implement triangle rasterization
-
+  glBegin(GL_TRIANGLES);
+  glColor4f(color.r, color.g, color.b, color.a);
+  glVertex2f(x0, y0);
+  glVertex2f(x1, y1);
+  glVertex2f(x2, y2);
+  glEnd();
 }
 
 void HardwareRenderer::rasterize_image(float x0, float y0,
